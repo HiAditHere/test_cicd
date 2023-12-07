c75ea4033e0ce7ea957ef8a208b56dbd14e17385
Adit Bhosle <aditbhosle81@gmail.com>
2023-12-07 03:20:51 -0500

    tr

--- test/unittest.py
+++ test/unittest.py
@@ -6,9 +6,6 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(project_root)
 class TestPipeline(unittest.TestCase):
 
-    def sample(self):
+    def test_sample(self):
         a = 3
         self.assertEqual(a,3)
-
-if __name__ == '__main__':
-    unittest.main()
